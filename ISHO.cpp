24e006cbf6885b0993830ce397562e8fcf8a3d2d
Kashashi <38377804+Kashashi@users.noreply.github.com>
2020-03-28 10:33:09 +0800

    ISHO records as not compiling
    win32 api Processing

--- ISHO.cpp
+++ ISHO.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include<conio.h>
 #include<string>
 #include <ctime>
 #include <thread>
+#include <tchar.h>
 using namespace std;
 
 int *runtime_code = new int(1); //執行代碼
 
 //檢查完畢
 
-float max_x = 40.0;//宣告最大範圍
-float max_y = 40.0;//宣告最大範圍
-float full_x = max_x;
-float full_y = max_y;
+double max_x = 40.0;//宣告最大範圍
+double max_y = 40.0;//宣告最大範圍
+double full_x = max_x;
+double full_y = max_y;
 
 //宣告延遲函數
 void Delay(int time)//time*1000為秒數
@@ -72,7 +72,7 @@ public:
     }
 };
 
-
+/*
 int main(){
     cout << "Hello! World!\n";
     cout << "地圖最大X軸:" << full_x << endl;
@@ -85,7 +85,55 @@ int main(){
     a.join();
     b.join();
 
+
     return 0;
+}*/
+#ifndef LINUX
+#define UNICODE
+#include <windows.h>
+//主窗口類名稱
+static char *szWindowClass[] = {"DesktopApp"};
+//出現在應用程序標題欄中的字串
+static char *szTitle[] = {"Windows Desktop Guided Tour Application"};
+HINSTANCE hInst;
+//轉發此代碼模塊中包含的函數的聲明：
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow)
+{
+    WNDCLASSEXA wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = "DesktopApp";
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    hInst = hInstance;//將實例句柄存儲在全局變量中
+    HWND hWnd = CreateWindow(// CreateWindow的參數說明：
+        szWindowClass,// szWindowClass：應用程序的名稱
+        szTitle,// szTitle：出現在標題欄中的文本
+        WS_OVERLAPPEDWINDOW,// WS_OVERLAPPEDWINDOW：要創建的窗口的類型
+        CW_USEDEFAULT, CW_USEDEFAULT,// CW_USEDEFAULT，CW_USEDEFAULT：初始位置（x，y）
+        500, 100,// 500、100：初始大小（寬度，長度）
+        NULL,// NULL：此窗口的父級
+        NULL,// NULL：此應用程序沒有菜單欄
+        hInstance,// hInstance：WinMain中的第一個參數
+        NULL// NULL：在此應用程序中不使用
+    );
+
+   ShowWindow(hWnd,// ShowWindow的參數說明：
+      nCmdShow);// hWnd：從CreateWindow返回的值
+   UpdateWindow(hWnd);// nCmdShow：WinMain中的第四個參數
 }
+
+
+#endif // LINUX
 
 
